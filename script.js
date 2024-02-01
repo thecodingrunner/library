78107292f9ca769f78bea60e8b7b6c1cb73d082a
Finn Moffett <finn.moffett@gmail.com>
2024-02-01 10:46:59 +0000

    Add items to library and make popup form option
    Added additional items of author and read to input. Made the form a
    popup. Will change this to a dialog instead for flexibility.

--- script.js
+++ script.js
@@ -2,18 +2,32 @@ let formInput = document.querySelector("#form")
 let para = document.querySelector("p")
 let button = document.querySelector("button")
 let table = document.querySelector("table")
+let formDisplay = document.querySelector(".display")
+// let input = document.querySelector("input")
+let popup = document.querySelector(".form-popup")
+
+
+function openForm() {
+  popup.style.display = "block";
+}
+
+function closeForm() {
+  popup.style.display = "none";
+}
 
 const myLibrary = [];
 
-function Book(name, pages) {
+function Book(author, name, pages, read) {
+  this.author = author;
   this.name = name;
   this.pages = pages;
+  this.read = read;
 }
 
 // const book2 = new Book('lord of the rings', 400)
 
-function addBookToLibrary(name, pages) {
-  let book = new Book(name, pages);
+function addBookToLibrary(author, name, pages, read) {
+  let book = new Book(author, name, pages, read);
   myLibrary.push(book);
 }
 
@@ -21,16 +35,25 @@ function addBookToLibrary(name, pages) {
 
 // addBookToLibrary('random', 5)
 
-formInput.addEventListener("submit", () => {
-  let nameInput = document.querySelector("#name").value
-  let pagesInput = document.querySelector("#pages").value
-  addBookToLibrary(nameInput, pagesInput)
-  // myLibrary.forEach((book) => {
+formInput.addEventListener("submit", (event) => {
+  event.preventDefault()
+  let authorInput = document.querySelector("#author").value;
+  let nameInput = document.querySelector("#name").value;
+  let pagesInput = document.querySelector("#pages").value;
+  let readInput = document.querySelector("#read").value;
+  addBookToLibrary(authorInput, nameInput, pagesInput, readInput);
+})
+
+formDisplay.addEventListener("click", () => {
+  myLibrary.forEach((book) => {
     let newRow = table.insertRow(1);
     let cell1 = newRow.insertCell(0);
     let cell2 = newRow.insertCell(1);
-    cell1.textContent += nameInput;
-    cell2.textContent += pagesInput;
-  // })
+    let cell3 = newRow.insertCell(2);
+    let cell4 = newRow.insertCell(3);
+    cell1.textContent += book.author;
+    cell2.textContent += book.name;
+    cell3.textContent += book.pages;
+    cell4.textContent += book.read;
+  })
 });
-
