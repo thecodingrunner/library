3288bdef90f7ed2eb85bf76a5dbd7895ee6127f4
Finn Moffett <finn.moffett@gmail.com>
2024-02-01 11:57:21 +0000

    Convert popup to animated dialog, add delete option
    Converted the popup to an animated dialof with a background picture of
    a bookshelf. Added a deleted option on the end of each row, that deletes
    the book information from the row and removes the book data from the
    myLibrary array. Could add a clear all option for the displayed table,
    or prevent table from adding more content if all are displayed.

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 let formInput = document.querySelector("#form")
 let para = document.querySelector("p")
-let button = document.querySelector("button")
+let closeForm = document.querySelector(".closeForm")
 let table = document.querySelector("table")
 let formDisplay = document.querySelector(".display")
 // let input = document.querySelector("input")
-let popup = document.querySelector(".form-popup")
+// let popup = document.querySelector(".form-popup")
+const dialog = document.querySelector("dialog");
+const openForm = document.querySelector(".openForm")
 
 
-function openForm() {
-  popup.style.display = "block";
-}
+openForm.addEventListener('click', () => {
+  dialog.showModal();
+});
 
-function closeForm() {
-  popup.style.display = "none";
-}
+
+closeForm.addEventListener('click', () => {
+  dialog.close();
+});
 
 const myLibrary = [];
 
@@ -51,9 +54,17 @@ formDisplay.addEventListener("click", () => {
     let cell2 = newRow.insertCell(1);
     let cell3 = newRow.insertCell(2);
     let cell4 = newRow.insertCell(3);
+    let cell5 = newRow.insertCell(4);
     cell1.textContent += book.author;
     cell2.textContent += book.name;
     cell3.textContent += book.pages;
     cell4.textContent += book.read;
+    let del = document.createElement("button");
+    del.textContent = "delete";
+    cell5.appendChild(del);
+    del.addEventListener('click', (book) => {
+      myLibrary.splice(myLibrary.indexOf(book));
+      newRow.remove();
+    })
   })
 });
