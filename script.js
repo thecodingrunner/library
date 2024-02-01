f1619b48afd272fc763ae4262127cd26589ca9ca
Finn Moffett <finn.moffett@gmail.com>
2024-02-01 16:05:30 +0000

    Format page and add style
    Formatted and added a background etc. Changed the form so that it closes
    when the confirm button is pressed. Completed most of functionality and
    basic layout.
    
    Potential improvements:
    data section below buttons displaying books read, books to read, pages
    read etc.
    Additional styling.
    Clear/hide table button/option.
    Option to add rating and description of book (number and text area
    inputs)
    Option to save data.
    Day and night mode options.
    Option to toggle between card and table format.

--- script.js
+++ script.js
@@ -3,18 +3,20 @@ let para = document.querySelector("p")
 let closeForm = document.querySelector(".closeForm")
 let table = document.querySelector("table")
 let formDisplay = document.querySelector(".display")
-// let input = document.querySelector("input")
-// let popup = document.querySelector(".form-popup")
 const dialog = document.querySelector("dialog");
 const openForm = document.querySelector(".openForm")
+const confirmForm = document.querySelector(".submit")
 
 
 openForm.addEventListener('click', () => {
   dialog.showModal();
 });
 
-
 closeForm.addEventListener('click', () => {
+  dialog.close();
+});
+
+confirmForm.addEventListener('click', () => {
   dialog.close();
 });
 
@@ -43,19 +45,19 @@ formInput.addEventListener("submit", (event) => {
   let authorInput = document.querySelector("#author").value;
   let nameInput = document.querySelector("#name").value;
   let pagesInput = document.querySelector("#pages").value;
-  let readInput = document.querySelector("#read").value;
+  let readInput = document.querySelector("#read").checked;
   addBookToLibrary(authorInput, nameInput, pagesInput, readInput);
-})
+}) 
 
 formDisplay.addEventListener("click", () => {
   myLibrary.forEach((book) => {
-    if (table.rows.length < myLibrary.length + 1) {
     let newRow = table.insertRow(1);
     let cell1 = newRow.insertCell(0);
     let cell2 = newRow.insertCell(1);
     let cell3 = newRow.insertCell(2);
     let cell4 = newRow.insertCell(3);
     let cell5 = newRow.insertCell(4);
+    let cell6 = newRow.insertCell(5);
     cell1.textContent += book.author;
     cell2.textContent += book.name;
     cell3.textContent += book.pages;
@@ -67,6 +69,12 @@ formDisplay.addEventListener("click", () => {
       myLibrary.splice(myLibrary.indexOf(book));
       newRow.remove();
     })
-    }
+    let read = document.createElement("input");
+    read.type = "checkbox";
+    cell6.appendChild(read);
+    read.addEventListener("change", () => {
+      book.read = !book.read;
+      cell4.textContent = book.read;
+    })
   })
 });
